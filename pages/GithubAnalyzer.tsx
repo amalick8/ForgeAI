e86a0f718c7755dcf879663245f5437968595710
Ammar Malick <ammarmalick2006@gmail.com>
2026-02-18 22:39:03 -0600

    fix: remove unnecessary blank line in GithubAnalyzer component

--- pages/GithubAnalyzer.tsx
+++ pages/GithubAnalyzer.tsx
@@ -49,7 +49,7 @@ const GithubAnalyzer: React.FC = () => {
             <Github size={20} />
         </button>
       </div>
-
+      
       {result && (
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 animate-in fade-in slide-in-from-bottom-4 duration-700">
             <div className="lg:col-span-1 space-y-6">
